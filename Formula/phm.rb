8ec81a36b2b2f3f40f17f4b00bff3b98c2ec9c80
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-04-09 21:04:26 +0200

    Update phm to v0.1.1

--- Formula/phm.rb
+++ Formula/phm.rb
@@ -1,17 +1,17 @@
 class Phm < Formula
   desc "Fast PHP version manager for macOS"
   homepage "https://github.com/Rovasch/phm"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_arm do
     url "https://github.com/Rovasch/phm/releases/download/v#{version}/phm-aarch64-apple-darwin.tar.gz"
-    sha256 "91c17f50b5e4e6001d1a87bbc143afac2e773cdb8c08816bdadbc5bfa40687f7"
+    sha256 "f6385bc2fb6ce0b067655712122dd00d1ee8dc7c81507f89cf42d1aa20527430"
   end
 
   on_intel do
     url "https://github.com/Rovasch/phm/releases/download/v#{version}/phm-x86_64-apple-darwin.tar.gz"
-    sha256 "baf724d67a1c5be27848bbf496ad61595a3a75dbe393daf503480114530571ee"
+    sha256 "cd1ad312d085672fc9d2084a9490c29ba34d5ba53590d458ddae42499d4ce0d6"
   end
 
   def install
