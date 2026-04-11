734635b265d91c5dd4c1bacac4feb51580e40e1f
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-04-11 13:55:40 +0200

    chore: bump phm to v0.2.0
    Interactive install prompt for missing PHP versions.

--- Formula/phm.rb
+++ Formula/phm.rb
@@ -1,17 +1,17 @@
 class Phm < Formula
   desc "Fast PHP version manager for macOS"
   homepage "https://github.com/Rovasch/phm"
-  version "0.1.2"
+  version "0.2.0"
   license "MIT"
 
   on_arm do
     url "https://github.com/Rovasch/phm/releases/download/v#{version}/phm-aarch64-apple-darwin.tar.gz"
-    sha256 "0d39ffcb14aa7fcced669fe8cfee4d0590a234c2fc8533a81e335c7cee3fb915"
+    sha256 "8a292063ec4354a0108b769f9abf06c78f0149e1fb7438ef79193df2b9862ad6"
   end
 
   on_intel do
     url "https://github.com/Rovasch/phm/releases/download/v#{version}/phm-x86_64-apple-darwin.tar.gz"
-    sha256 "9f6e44a60839c0cda8fffb65fa479f950f5cb81b98a205e8f6f248f6bea94c7a"
+    sha256 "8250fdc6ee229067c2e69350d2bfb54b3017b8729c47f382d64a3bcb096d4b8e"
   end
 
   def install
